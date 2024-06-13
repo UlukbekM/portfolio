7318c01a2c6529cf91b1f4439d60f4c006190c1f
UlukbekM <44413738+UlukbekM@users.noreply.github.com>
2024-06-13 16:45:43 -0400

    google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import Header from './Header/header'
 import { Providers } from './Providers'
 import { Analytics } from '@vercel/analytics/react';
 import Footer from './footer'
-
+import { GoogleTagManager } from '@next/third-parties/google'
 // https://tailwind-nextjs-starter-blog.vercel.app/blog
 
 const inter = Inter({ subsets: ['latin'] })
@@ -31,6 +31,17 @@ export default function RootLayout({
                 <link rel="shortcut icon" href="/favicon.ico" />
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"/>
             </head>
+            <GoogleTagManager gtmId="G-4H10EQ1GVE" />
+            {/* <!-- Google tag (gtag.js) --> */}
+            {/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-4H10EQ1GVE"></script>
+            <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-4H10EQ1GVE');
+            </script> */}
+
             <body className={`${prompt.className} overflow-y-scroll` }>
                 <Providers>
                     <Header/>
